ccb0b63a2b04e41e4954efd3056b049be7976619
Zukhriddinbek-Code <zuhriddinganiyev2000@gmail.com>
2023-06-30 21:33:27 +0900

    working on SongCard component to render the songcard images and etc

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
-import { useSelector } from 'react-redux';
-import { Route, Routes } from 'react-router-dom';
+/* eslint-disable quotes */
+/* eslint-disable no-unused-vars */
 
-import { Searchbar, Sidebar, MusicPlayer, TopPlay } from './components';
-import { ArtistDetails, TopArtists, AroundYou, Discover, Search, SongDetails, TopCharts } from './pages';
+import { useSelector } from "react-redux";
+import { Route, Routes } from "react-router-dom";
+
+import { Searchbar, Sidebar, MusicPlayer, TopPlay } from "./components";
+import {
+  ArtistDetails,
+  TopArtists,
+  AroundYou,
+  Discover,
+  Search,
+  SongDetails,
+  TopCharts,
+} from "./pages";
 
 const App = () => {
   const { activeSong } = useSelector((state) => state.player);
--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable quotes */
+/* eslint-disable no-unused-vars */
 import { Error, Loader, SongCard } from "../components";
 import { genres } from "../assets/constants";
 import { useGetTopChartsQuery } from "../redux/services/shazamCore";
