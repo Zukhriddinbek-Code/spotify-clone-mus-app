1b87ec4b8a6c5c7eadb92b3ce5e7e28e76cd7ebf
Zukhriddinbek-Code <zuhriddinganiyev2000@gmail.com>
2023-07-02 13:10:48 +0900

    side bar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,50 @@
-const Sidebar = () => (
-  <div>Sidebar</div>
-);
+/* eslint-disable array-callback-return */
+/* eslint-disable react/self-closing-comp */
+/* eslint-disable react/jsx-no-useless-fragment */
+/* eslint-disable arrow-body-style */
+/* eslint-disable no-useless-return */
+/* eslint-disable quotes */
+/* eslint-disable no-unused-vars */
+
+import { useState } from "react";
+import { NavLink } from "react-router-dom";
+import { RiCloseLine } from "react-icons/ri";
+import { HiOutlineMenu } from "react-icons/hi";
+import { logo } from "../assets";
+import { links } from "../assets/constants";
+
+const Navlinks = ({ handleClick }) => {
+  return (
+    <div className="mt-10">
+      {links.map((link) => (
+        <NavLink
+          key={link.name}
+          to={link.to}
+          className="flex flex-row justify-start items-center my-8 text-sm font-medium text-gray-400 hover:text-cyan-400"
+          end
+          onClick={handleClick && handleClick()}
+        >
+          <link.icon className="w-6 h-6 mr-2" />
+          {link.name}
+        </NavLink>
+      ))}
+    </div>
+  );
+};
+
+const Sidebar = () => {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+  return (
+    <>
+      <div className="md:flex hidden flex-col w-[240px] py-10 px-4 bg-[#191624]">
+        <img src={logo} alt="logo" className="w-full h-14 object-contain" />
+        <Navlinks />
+      </div>
+
+      <div>{mobileMenuOpen ? <RiCloseLine /> : <HiOutlineMenu />}</div>
+    </>
+  );
+};
 
 export default Sidebar;
