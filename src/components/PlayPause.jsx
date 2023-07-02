c8a324aff2ded7d0fdfc3a91e939f5399391214d
Zukhriddinbek-Code <zuhriddinganiyev2000@gmail.com>
2023-07-02 10:55:13 +0900

    adding icons to the active song || isPlaying song

--- src/components/PlayPause.jsx
+++ src/components/PlayPause.jsx
@@ -1,5 +1,16 @@
-const PlayPause = () => (
-  <div>Loader</div>
-);
+/* eslint-disable no-confusing-arrow */
+/* eslint-disable implicit-arrow-linebreak */
+/* eslint-disable jsx-a11y/anchor-is-valid */
+/* eslint-disable react/self-closing-comp */
+/* eslint-disable quotes */
+/* eslint-disable no-unused-vars */
 
+import { FaPauseCircle, FaPlayCircle } from "react-icons/fa";
+
+const PlayPause = ({ isPlaying, activeSong, song, handlePause, handlePlay }) =>
+  isPlaying && activeSong?.title === song.title ? (
+    <FaPauseCircle size={35} className="text-gray-300" onClick={handlePause} />
+  ) : (
+    <FaPlayCircle size={35} className="text-gray-300" onClick={handlePlay} />
+  );
 export default PlayPause;
