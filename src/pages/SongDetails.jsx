caf1ff6d32391d9cbde34ca5276c45eb51718ab9
Zukhriddinbek-Code <zuhriddinganiyev2000@gmail.com>
2023-07-07 17:12:19 +0900

    using endpoints, we are creating our own api for song details and rendering the lyrics

--- src/pages/SongDetails.jsx
+++ src/pages/SongDetails.jsx
@@ -9,20 +9,35 @@ import { useSelector, useDispatch } from "react-redux";
 import { DetailsHeader, Error, Loader, RelatedSongs } from "../components";
 
 import { setActiveSong, playPause } from "../redux/features/playerSlice";
+import { useGetSongDetailsQuery } from "../redux/services/shazamCore";
 
 const SongDetails = () => {
   const dispatch = useDispatch();
   const { songid } = useParams();
   const { activeSong, isPlaying } = useSelector((state) => state.player);
+  const { data: songData, isFetching: isFetchingSongDetails } =
+    useGetSongDetailsQuery({ songid });
+
   console.log(songid);
 
   return (
     <div className="flex flex-full">
-      {/* <DetailsHeader artistId={artistId} songData={songid.data} /> */}
+      <DetailsHeader artistId="" songData={songid.data} />
 
       <div className="mb-10">
         <h2 className="text-white text-3xl font-bold">Lyrics:</h2>
-        <div className="mt-5"></div>
+
+        <div className="mt-5">
+          {songData?.sections[1].type === "LYRICS" ? (
+            songData?.sections[1].text.map((line, i) => (
+              <p className="text-gray-400 text-base my-1">{line}</p>
+            ))
+          ) : (
+            <p className="text-gray-400 text-base my-1">
+              Sorry, no lyrics found!
+            </p>
+          )}
+        </div>
       </div>
     </div>
   );
--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -17,10 +17,13 @@ export const shazamCoreApi = createApi({
   }),
   endpoints: (builder) => ({
     getTopCharts: builder.query({ query: () => "/charts/world" }),
+    getSongDetails: builder.query({
+      query: ({ songid }) => `/tracks/details?track_id=${songid}`,
+    }),
   }),
 });
 
-export const { useGetTopChartsQuery } = shazamCoreApi;
+export const { useGetTopChartsQuery, useGetSongDetailsQuery } = shazamCoreApi;
 
 /*
 const options = {
