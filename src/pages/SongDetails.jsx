d0e98131da2b879b9e7e7fba7a5d81efc5bdc655
Zukhriddinbek-Code <zuhriddinganiyev2000@gmail.com>
2023-07-08 18:57:29 +0900

    rendering song details and Related Songs

--- src/pages/SongDetails.jsx
+++ src/pages/SongDetails.jsx
@@ -9,7 +9,10 @@ import { useSelector, useDispatch } from "react-redux";
 import { DetailsHeader, Error, Loader, RelatedSongs } from "../components";
 
 import { setActiveSong, playPause } from "../redux/features/playerSlice";
-import { useGetSongDetailsQuery } from "../redux/services/shazamCore";
+import {
+  useGetSongDetailsQuery,
+  useGetSongRelatedQuery,
+} from "../redux/services/shazamCore";
 
 const SongDetails = () => {
   const dispatch = useDispatch();
@@ -18,10 +21,33 @@ const SongDetails = () => {
   const { data: songData, isFetching: isFetchingSongDetails } =
     useGetSongDetailsQuery({ songid });
 
-  console.log(songid);
+  const handlePauseClick = () => {
+    dispatch(playPause(false));
+  };
+
+  const handlePlayClick = (song, i) => {
+    dispatch(setActiveSong({ song, data, i }));
+    dispatch(playPause(true));
+  };
+
+  const {
+    data,
+    isFetching: isFetchingRelatedSong,
+    error,
+  } = useGetSongRelatedQuery({
+    songid,
+  });
+
+  if (isFetchingRelatedSong || isFetchingSongDetails) {
+    return <Loader title="Searching song details" />;
+  }
+
+  if (error) {
+    return <Error />;
+  }
 
   return (
-    <div className="flex flex-full">
+    <div className="flex flex-col">
       <DetailsHeader artistId="" songData={songid.data} />
 
       <div className="mb-10">
@@ -30,7 +56,9 @@ const SongDetails = () => {
         <div className="mt-5">
           {songData?.sections[1].type === "LYRICS" ? (
             songData?.sections[1].text.map((line, i) => (
-              <p className="text-gray-400 text-base my-1">{line}</p>
+              <p key={i} className="text-gray-400 text-base my-1">
+                {line}
+              </p>
             ))
           ) : (
             <p className="text-gray-400 text-base my-1">
@@ -39,6 +67,14 @@ const SongDetails = () => {
           )}
         </div>
       </div>
+
+      <RelatedSongs
+        data={data}
+        isPlaying={isPlaying}
+        activeSong={activeSong}
+        handlePauseClick={handlePauseClick}
+        handlePlayClick={handlePlayClick}
+      />
     </div>
   );
 };
